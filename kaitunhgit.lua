76e744308bf72a027ca01d8d6b15e792760f06d3
piyawat12345 <inwnutza@hotmail.com>
2025-07-09 22:06:34 +0700

    Update kaitunhgit.lua

--- kaitunhgit.lua
+++ kaitunhgit.lua
@@ -1,4 +1,4 @@
-_G.Marukey = "MARU-L8FE-MDUQN-49QO-0YRY3-2JFJ"
-_G.Maruid  = "1200746991039287327"
+_G.Marukey = "MARU-YRDY7-VYSXC-NQYX-673HG-G01DD"
+_G.Maruid  = "285324993133543425"
 _G.Api = "http://localhost:8899/"
 loadstring(game:HttpGet("https://api.genhubs.com/v2/loadscriptkaitun/?script_key=Vcfm0uDBvASKzIfoexHgsMXYLvdEi3Vz", true))()
