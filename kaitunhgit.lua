afbcd44691023cce7aa38f3d303a6938ecbb8f01
piyawat12345 <inwnutza@hotmail.com>
2025-07-09 22:01:43 +0700

    Create kaitunhgit.lua

---
+++ kaitunhgit.lua
@@ -0,0 +1,4 @@
+_G.Marukey = "MARU-L8FE-MDUQN-49QO-0YRY3-2JFJ"
+_G.Maruid  = "1200746991039287327"
+_G.Api = "http://localhost:8899/"
+loadstring(game:HttpGet("https://api.genhubs.com/v2/loadscriptkaitun/?script_key=Vcfm0uDBvASKzIfoexHgsMXYLvdEi3Vz", true))()
