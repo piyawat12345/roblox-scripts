dc291d688b4e1aaeec053727f8d7b38afc512c11
piyawat12345 <inwnutza@hotmail.com>
2025-08-09 20:41:17 +0700

    Update kaitun.lua

--- kaitun.lua
+++ kaitun.lua
@@ -1,30 +1,22 @@
 getgenv().ConfigsKaitun = {
-	Beta_Fix_Data_Sync = true,
-
 	["Block Pet Gift"] = true,
-
-	Collect_Cooldown = 60, -- cooldown to collect fruit
-	JustFuckingCollectAll = false, -- Collect all (fruit not wait mutation)
-
+	
 	["Low Cpu"] = true,
 	["Auto Rejoin"] = true,
-
+	
 	["Rejoin When Update"] = false,
 	["Limit Tree"] = {
-		["Limit"] = 300,
-		["Destroy Untill"] = 280,
+		["Limit"] = 200,
+		["Destroy Untill"] = 150,
 
 		["Safe Tree"] = {
-			"Moon Blossom",
-			"Fossilight",
 		}
 	},
 
 	Seed = {
 		Buy = {
 			Mode = "Auto", -- Custom , Auto
-			Custom = { -- any fruit u need to place
-				"Carrot",
+			Custom = {
 			}
 		},
 		Place = {
@@ -53,101 +45,103 @@ getgenv().ConfigsKaitun = {
 	},
 
 	Events = {
-		["Cook Event"] = {
-			Minimum_Money = 30_000_000, -- minimum money to start play this event
+		["Zen Event"] = {
+			["Restocking"] = { -- Minimumthing to restock
+				Max_Restocks_Price = 3_000_000,
+				Minimum_Money = 500_000,
+				Minimum_Chi = 30
+			},
+			["Doing"] = {
+				Minimum_Money = 1000, -- minimum money to start play this event
+				First_Upgrade_Tree = 1,
+				Maximum_Chi = 50,
+			}
 		},
 		["Traveling Shop"] = {
 			"Bee Egg",
 		},
 		Craft = {
-			"Ancient Seed Pack",
-			"Anti Bee Egg",
 			"Primal Egg",
+			"Anti Bee Egg",
+			"Small Toy",
+			"Small Treat",
+			"Ancient Seed Pack",
 		},
 		Shop = {
 			"Zen Egg",
-			"Zen Seed Pack",
-			"Spiked Mango",
 			"Pet Shard Tranquil",
 			"Pet Shard Corrupted",
-			"Koi",
-			"Soft Sunshine",
-			"Sakura Bush",
-			"Raiju",
 		},
-		Start_Do_Honey = 2_000_000 -- start trade fruit for honey at money
 	},
 
 	Gear = {
 		Buy = { 
+			"Watering Can",
+			"Trowel",
+			"Recall Wrench",
+			"Magnifying Glass",
+			"Tanning Mirror",
+			"Cleaning Spray",
+			"Favorite Tool",
+			"Harvest Tool",
+			"Friendship Pot",
 			"Master Sprinkler",
+			"Basic Sprinkler",
 			"Godly Sprinkler",
 			"Advanced Sprinkler",
-			"Basic Sprinkler",
-			"Lightning Rod",
-			"Level Up Lollipop",
-			"Medium Treat",
 			"Medium Toy",
+			"Medium Treat",
+			"Levelup Lollipop",
+			"Lightning Rod",
 		},
 		Lock = {
-			"Master Sprinkler",
 			"Godly Sprinkler",
 			"Advanced Sprinkler",
 			"Basic Sprinkler",
-			"Lightning Rod",
 		},
 	},
 
 	Eggs = {
 		Place = {
-			"Gourmet Egg",
 			"Zen Egg",
-			"Primal Egg",
-			"Dinosaur Egg",
-			"Oasis Egg",
+			"Common Summer Egg",
 			"Anti Bee Egg",
-			"Night Egg",
-			"Bug Egg",
-			"Paradise Egg",
 			"Bee Egg",
+			"Bug Egg",
 			"Rare Summer Egg",
+			"Paradise Egg",
+			"Night Egg",
 			"Mythical Egg",
-			"Common Egg",
 		},
 		Buy = {
-			"Bee Egg",
-			"Oasis Egg",
-			"Paradise Egg",
 			"Anti Bee Egg",
+			"Bee Egg",
 			"Night Egg",
-			"Rare Summer Egg",
 			"Bug Egg",
+			"Paradise Egg",
 			"Mythical Egg",
-			"Common Egg",
+			"Rare Egg",
+			"Rare Summer Egg",
+			"Common Summer Egg",
 		}
 	},
 
 	Pets = {
-		["Start Delete Pet At"] = 40,
+		["Start Delete Pet At"] = 50,
 		["Upgrade Slot"] = {
 			["Pet"] = {
-				["Starfish"] = { 5, 100, 1, true }, -- the "true" on the last is auto equip (use for like only need to use for upgrade pet)
+				["Starfish"] = { 8, 100, 1 },
 			},
-			["Limit Upgrade"] = 2, -- max is 5 (more than or lower than 1 will do nothing)
+			["Limit Upgrade"] = 5,
 			["Equip When Done"] = {
-				["Tanchozuru"] = { 5, 100, 1 }, -- 5 on the first mean equip only 5 | pet , 100 mean equip only level pet lower than 100 | the one on the last is priority it will ues first if possible 
-				["Ostrich"] = { 3, 100, 2 },
-				["Blood Kiwi"] = { 8, 100 },
-				["Seal"] = { 8, 100 },
-				["Rooster"] = { 8, 100 },
-				["Starfish"] = { 5, 75 },
+				["Blood Kiwi"] = { 8, 100, 2 },
+				["Rooster"] = { 5, 100, 3 },
+				["Tanchozuru"] = { 8, 100, 4 },
+				["Kodama"] = { 2, 100, 1 },
 			},
 		},
-		Favorite_LockedPet = true,
-		Locked_Pet_Age = 60, -- pet that age > 60 will lock
+		Locked_Pet_Age = 50, -- pet that age > 60 will lock
 		Locked = {
-			"French Fry Ferret",
-			"Spaghetti Sloth",
 			"Corrupted Kitsune",
 			"Raiju",
 			"Koi",
@@ -156,8 +150,6 @@ getgenv().ConfigsKaitun = {
 			"Kappa",
 			"Kitsune",
 			"Dilophosaurus",
-			"Moon Cat",
-			"Capybara",
 			"Spinosaurus",
 			"Bear Bee",
 			"T-Rex",
@@ -171,39 +163,48 @@ getgenv().ConfigsKaitun = {
 			"Mimic Octopus",
 			"Red Fox",
 			"Blood Owl",
-			["Starfish"] = 10,
+			["Starfish"] = 8,
 		},
-		LockPet_Weight = 7, -- if Weight >= 10 they will locked
+		LockPet_Weight = 5, -- if Weight >= 10 they will locked,
+		Instant_Sell = {		
+			"Shiba Inu",
+		}
 	},
 
 	Webhook = {
-		UrlPet = "xxx",
-		UrlSeed = "xxx",
-		PcName = "xxx",
+		UrlPet = "XXX",
+		UrlSeed = "XXX",
+		PcName = "LUCKY-AYAYA",
 
 		Noti = {
 			Seeds = {
 				"Sunflower",
 				"Dragon Pepper",
 				"Elephant Ears",
+				"Bone Blossom",
+				"Dragon Sapling",
+                "Maple Apple",
 			},
 			SeedPack = {
 				"Idk"
 			},
 			Pets = {
-				"French Fry Ferret",
-				"Corrupted Kitsune",
+				"Tanchozuru",
 				"Kitsune",
-				"Spinosaurus",
+				"Kappa",
 				"T-Rex",
+				"Brontosaurus",
 				"Disco Bee",
 				"Butterfly",
 				"Mimic Octopus",
-				"Queen Bee",
 				"Fennec Fox",
+				"Queen Bee",
 				"Dragonfly",
 				"Raccoon",
 				"Red Fox",
+				"Dilophosaurus",
+				"Ankylosaurus",
+				"Spinosaurus",
 			},
 			Pet_Weight_Noti = true,
 		}
